9948237bb77bb7472d8472e9c332bb2c8021a936
satvik628 <satvikwhitehat@gmail.com>
2020-10-14 13:39:18 +0530

    added a platform

--- sketch.js
+++ sketch.js
@@ -33,6 +33,8 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    platform=new Ground(100,300,200,300);
+
     bird = new Bird(100,100);
 
 }
@@ -57,6 +59,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+ platform.display();
 
     bird.display();
 }
